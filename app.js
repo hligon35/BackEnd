542d529f852aba436476f170a808b54fb99a7a94
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2025-02-24 15:08:24 -0500

    started M06

--- app.js
+++ app.js
@@ -2,34 +2,32 @@
 const express = require("express")
 var cors = require('cors')
 //activate or tell this app variable to be an express server
+
+
+const bodyParser = require("body-parser")
+const Song = require("./models/songs")
 const app = express()
 app.use(cors())
+
+app.use(body-parser.json())
 const router = express.Router()
 
-
-//making an api using routes
-//Routes are used to handle browser requests. They look like URLs. The difference is that when a browser requests a route, it is dynamically handled by using a function.
-
+//grab all songs in a database
 router.get("/songs", function(req, res){
-    const songs = [
-        {
-            title: "We Found Love",
-            artist: "Rihanna",
-            popularity: 10,
-            releaseDate: new Date(2011, 9, 22),
-            genre: ["electro house"]
-        },
-        {
-            title: "Happy",
-            artist: "Pharrell Williams",
-            popularity: 10,
-            releaseDate: new Date(2013, 11, 21),
-            genre: ["soul", "new soul"]
+    let query = {}
+    if(req.query.genre){
+        query = {genre : req.query.genre}
+    }
+
+    //to find all songs in the databaseyou just use the find method that is built into mongodb
+    Song.find(query, function(err, songs){
+        if(err){
+            res.status(400).send(err)
         }
-    ];
-
-
-    res.json(songs)
+        else{
+            res.json(songs)
+        }
+    })
 })
 
 //all request that usually use an api start with /api...so the url would be localhost:3000/api/songs
---
+++ models/songs.js
@@ -0,0 +1,10 @@
+const db = require("../db")
+
+const song = db.model("Song", {
+    title: {type:String, required:true}, 
+    artist: String,
+    popularity: {type:Number, min:1, max:10},
+    releaseDate: {type:Date, default:Date.now},
+    genre: {String}
+})
+module.exports = song
